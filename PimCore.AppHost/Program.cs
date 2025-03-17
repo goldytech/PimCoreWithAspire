c6eb3635efa9c1858ea625fc3411bd98a19e890f
goldytech <afzal.qureshi@outlook.com>
2025-03-17 11:12:00 +0530

    Remove project references for ApiService and Web from solution and project files

--- PimCore.AppHost/Program.cs
+++ PimCore.AppHost/Program.cs
@@ -3,11 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var redis = builder.AddContainer("redis", "redis:alpine")
     .WithArgs("redis-server", "--maxmemory", "128mb", "--maxmemory-policy", "volatile-lru", "--save", "\"\"")
     .WithVolume("pimcore-demo-redis-data", "/data");
-var apiService = builder.AddProject<Projects.PimCore_ApiService>("apiservice");
 
-builder.AddProject<Projects.PimCore_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    .WaitFor(apiService);
 
 builder.Build().Run();
