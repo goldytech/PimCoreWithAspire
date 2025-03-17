218015767263087f57db9c3ca16a3e24cfa0e93b
goldytech <afzal.qureshi@outlook.com>
2025-03-17 11:59:48 +0530

    Add MariaDB container configuration to Program.cs

--- PimCore.AppHost/Program.cs
+++ PimCore.AppHost/Program.cs
@@ -4,5 +4,15 @@ var redis = builder.AddContainer("redis", "redis:alpine")
     .WithArgs("redis-server", "--maxmemory", "128mb", "--maxmemory-policy", "volatile-lru", "--save", "\"\"")
     .WithVolume("pimcore-demo-redis-data", "/data");
 
+// Mariadb
+var db = builder.AddContainer("db", "mariadb:10.11")
+    .WithArgs("mysqld", "--character-set-server=utf8mb4", "--collation-server=utf8mb4_general_ci",
+        "--innodb-file-per-table=1", "--log-bin=mysql-bin")
+    .WithVolume("pimcore-demo-database", "/var/lib/mysql")
+    .WithEnvironment("MYSQL_ROOT_PASSWORD", "ROOT")
+    .WithEnvironment("MYSQL_DATABASE", "pimcore")
+    .WithEnvironment("MYSQL_USER", "pimcore")
+    .WithEnvironment("MYSQL_PASSWORD", "pimcore");
+    
 
 builder.Build().Run();
